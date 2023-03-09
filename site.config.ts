42e4104f4234943edd49f1d544796e55c6e69c45
Ducduong1811 <70619961+Ducduong1811@users.noreply.github.com>
2023-03-09 14:34:08 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,28 +3,28 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   // rootNotionPageId: 'niviki-com-t-do-t-i-ch-nh-10f5e54221eb4b059bc5b5305da949ba',
-  rootNotionPageId: 'Simple-Template-49955b133eb843bdb8665534ecfd844c',
+  rootNotionPageId: 'Ahihitemplate-667c6cb5914c4c0c9d3ba91c6f85c5f9',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'NIVIKI.COM - TỰ DO TÀI CHÍNH',
-  domain: 'niviki.com',
-  author: 'Khoa Nguyen',
+  name: 'DUONGDUC.COM - RESEARCH PROJECTS',
+  domain: 'duongduc1811.com',
+  author: 'Duong Duc',
 
   // open graph metadata (optional)
-  description: 'Lan toả tinh thần tự do tài chính đến với nhiều người hơn nữa! Tự do tài chính là bạn được quyền “thất nghiệp” nhưng vẫn có đủ tiền cho một cuộc sống thoải mái trong thời gian dài',
+  description: 'As a place for Duong Duc to share his research projects',
 
   // social usernames (optional)
-  twitter: 'khoa_nvk',
+  // twitter: 'khoa_nvk',
   // github: 'transitive-bullshit',
-  linkedin: 'khoanguyen08',
+  linkedin: 'duc-duong-451991167',
   
-  facebook: 'https://www.facebook.com/groups/nocodevietnam',
+  //facebook: 'https://www.facebook.com/groups/nocodevietnam',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  //youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -69,8 +69,8 @@ export default siteConfig({
       pageId: '8e761b9e7d1b4db1ae76bbc1e4f9aa1f'
     },    
     {
-      title: 'About',
-      pageId: '38979ee661fc4a5d8af2d7c32319ee0d'
+      title: 'Research',
+      pageId: 'Researchs-acaf946c8c3442aca357b499f7728d5a?pvs=4'
     }
   ]
 })
